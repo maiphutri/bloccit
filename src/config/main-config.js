99219f03d00dcf813a2450aaeca7ddb9a16a7a35
maiphutri <mp.ritz@yahoo.com.vn>
2019-03-20 16:49:48 -0700

    Checkpoint 4 Resource2: Completed

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const path       = require('path'),
-      viewFolder = path.join(__dirname, "..", "views");
+      viewFolder = path.join(__dirname, "..", "views"),
+      bodyParser = require('body-parser');
 
 module.exports = {
   init(app, express) {
     app.set("views", viewFolder);
     app.set("view engine", "ejs");
-    app.use(express.static(path.join(__dirname, "..", "assests")))
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(express.static(path.join(__dirname, "..", "assets")))
   }
 }
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -8,5 +8,58 @@ module.exports = {
     .catch((err) => {
       callback(err);
     })
+  },
+
+  addTopic(newTopic, callback) {
+    return Topics.create({
+      title: newTopic.title,
+      description: newTopic.description
+    })
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+
+  getTopic(id, callback) {
+    return Topics.findByPk(id).then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+
+  deleteTopic(id, callback) {
+    return Topics.destroy({
+      where: {id}
+    })
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+
+  updateTopic(id, updatedTopic, callback) {
+    return Topics.findByPk(id)
+    .then((topic) => {
+      if (!topic) {
+        return callback("Topic not found");
+      }
+
+      topic.update(updatedTopic, {
+        fields: Object.keys(updatedTopic)
+      })
+      .then(() => {
+        callback(null, topic);
+      })
+      .catch((err) => {
+        callback(err);
+      })
+    })
   }
 }
