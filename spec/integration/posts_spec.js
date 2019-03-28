e0e0c7af2c1db006e677677a3791a38e037afa32
maiphutri <mp.ritz@yahoo.com.vn>
2019-03-28 15:03:40 -0700

    Checkpoint 7 Validations: Completed

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -75,6 +75,30 @@ describe("routes : posts", () => {
         }
       )
     });
+
+    it("should not create a new post that fails validations", (done) => {
+      const options = {
+        url: `${base}${this.topic.id}/posts/create`,
+        form: {
+          title: "a",
+          body: "b"
+        }
+      };
+
+      request.post(options,
+        (err, res, body) => {
+
+          Post.findOne({ where: {title: "a"} }).then(post => {
+            expect(post).toBeNull();
+            done();
+          })
+          .catch(err => {
+            console.log(err);
+            done();
+          })
+        }
+      )
+    })
   });
 
   describe("GET /topics/:topicId/posts/:id", () => {
@@ -140,7 +164,8 @@ describe("routes : posts", () => {
       const options = {
         url: `${base}${this.topic.id}/posts/${this.post.id}/update`,
         form: {
-          title: "Snowman Building Competition"
+          title: "Snowman Building Competition",
+          body: "ten characters long"
         }
       };
 
@@ -150,6 +175,7 @@ describe("routes : posts", () => {
           
           Post.findOne({ where: {id: this.post.id} }).then(post => {
             expect(post.title).toBe("Snowman Building Competition");
+            expect(post.body).toBe("ten characters long");
             done();
           });
         }
--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,13 +1,24 @@
 require('dotenv').config();
-const path       = require('path'),
-      viewFolder = path.join(__dirname, "..", "views"),
-      bodyParser = require('body-parser');
+const path             = require('path'),
+      viewFolder       = path.join(__dirname, "..", "views"),
+      bodyParser       = require('body-parser'),
+      expressValidator = require('express-validator'),
+      session          = require('express-session'),
+      flash            = require('express-flash');
 
 module.exports = {
   init(app, express) {
     app.set("views", viewFolder);
     app.set("view engine", "ejs");
     app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(express.static(path.join(__dirname, "..", "assets")))
+    app.use(express.static(path.join(__dirname, "..", "assets")));
+    app.use(expressValidator());
+    app.use(session({
+      secret: process.env.cookieSecret,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 60000 }
+    }));
+    app.use(flash());
   }
 }
