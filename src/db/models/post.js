d91579ed68befe5eccd668a3ab2e7635d0e466ce
maiphutri <mp.ritz@yahoo.com.vn>
2019-04-10 14:46:09 -0700

    Assignment 12 Favoriting: Completed

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -45,6 +45,13 @@ module.exports = (sequelize, DataTypes) => {
         userId: post.userId,
         postId: post.id
       })
+    });
+    Post.afterCreate((post, callback) => {
+      return models.Vote.create({
+        value: 1,
+        userId: post.userId,
+        postId: post.id
+      })
     })
   };
   Post.prototype.getPoints = function() {
